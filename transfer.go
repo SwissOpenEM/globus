1e306dbec5170dc11b3b2b665a72176953187eb1
consolethinks <consolethinks@gmail.com>
2024-05-07 17:11:56 +0200

    wip 2

--- transfer.go
+++ transfer.go
@@ -10,6 +10,78 @@ import (
 
 const transferBaseUrl = "https://transfer.api.globusonline.org/v0.10"
 
+// request structures
+
+type TransferItem struct {
+	DataType        string `json:"DATA_TYPE"` // can be either "tranfer_item" OR "transfer_symlink_item"
+	SourcePath      string `json:"source_path"`
+	DestinationPath string `json:"destination_path"`
+	// optionals
+	Recursive         *bool   `json:"recursive,omitempty"`
+	ExternalChecksum  *string `json:"external_checksum,omitempty"`
+	ChecksumAlgorithm *string `json:"checksum_algorithm,omitempty"`
+}
+
+type DeleteItem struct {
+	DataType string `json:"DATA_TYPE"` // always delete_item
+	Path     string `json:"path"`
+}
+
+type FilterRule struct {
+	DataType string `json:"DATA_TYPE"` // = filter_rule
+	Method   string `json:"method"`
+	Type     string `json:"type"`
+	Name     string `json:"name"`
+}
+
+type CommonTransfer struct {
+	DataType     string `json:"DATA_TYPE"` // = transfer OR delete
+	SubmissionId string `json:"submission_id"`
+	Label        string `json:"label,omitempty"`
+	// optional fields
+	NotifyOnSucceeded   *bool   `json:"notify_on_succeeded,omitempty"`
+	NotifyOnFailed      *bool   `json:"notify_on_failed,omitempty"`
+	NotifyOnInactive    *bool   `json:"notify_on_inactive,omitempty"`
+	SkipActivationCheck *bool   `json:"skip_activation_check,omitempty"`
+	Deadline            *string `json:"deadline,omitempty"`
+	StoreBasePathInfo   *bool   `json:"store_base_path_info,omitempty"`
+}
+
+type Transfer struct {
+	CommonTransfer
+	SourceEndpoint      string         `json:"source_endpoint"`
+	DestinationEndpoint string         `json:"destination_endpoint"`
+	Data                []TransferItem `json:"DATA"`
+	// optionals
+	FilterRules            *[]FilterRule `json:"filter_rules,omitempty"`
+	EncryptData            *bool         `json:"encrypt_data,omitempty"`             // default: false
+	SyncLevel              *int          `json:"sync_level,omitempty"`               //
+	VerifyChecksum         *bool         `json:"verify_checksum,omitempty"`          // default: false
+	PreserveTimestamp      *bool         `json:"preserve_timestamp,omitempty"`       // default: false
+	DeleteDestinationExtra *bool         `json:"delete_destination_extra,omitempty"` // default: false
+	SkipSourceErrors       *bool         `json:"skip_source_errors,omitempty"`       // default: ?
+	FailOnQuotaErrors      *bool         `json:"fail_on_quota_errors,omitempty"`     // default: ?
+	SourceLocalUser        *string       `json:"source_local_user,omitempty"`
+	DestinationLocalUser   *string       `json:"destination_local_user,omitempty"`
+	// some BETA or experimental optional fields that are omitted:
+	//  - RecursiveSymlinks
+	//  - perf_cc, perf_p, perf_pp
+	//  - perf_udt
+}
+
+type Delete struct {
+	CommonTransfer
+	Endpoint string `json:"endpoint"`
+	Data     []DeleteItem
+	// optionals
+	Recursive      *bool   `json:"recursive,omitempty"`       // default: false, required if any item is a directory
+	IgnoreMissing  *bool   `json:"ignore_missing,omitempty"`  // default: false
+	InterpretGlobs *bool   `json:"interpret_globs,omitempty"` // default: false
+	LocalUser      *string `json:"local_user,omitempty"`
+}
+
+// response structures
+
 type SubmissionId struct {
 	DataType string `json:"DATA_TYPE"`
 	Value    string `json:"value"`
@@ -19,7 +91,8 @@ type Link struct {
 	DataType string `json:"DATA_TYPE"`
 	Href     string `json:"href"`
 	Rel      string `json:"rel"`
-	// TODO: FINISH THIS!!!
+	Resource string `json:"resource"`
+	Title    string `json:"title"`
 }
 
 type TransferResult struct {
@@ -29,6 +102,7 @@ type TransferResult struct {
 	Resource     string `json:"resource"`
 	SubmissionId string `json:"submission_id"`
 	TaskId       string `json:"task_id"`
+	TaskLink     Link   `json:"task_link"`
 }
 
 func TransferSubmitTask(client *http.Client, sourceEndpoint string, sourcePath string, destEndpoint string, destPath string) (err error) {
